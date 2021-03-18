a236680b805444d9c5692c0102422e6eb46f51f0
oscarbonilla-tid <oscar.bonillagonzalez@telefonica.com>
2021-03-18 12:27:23 +0100

    chore: retry get driver pod

--- resource-managers/kubernetes/core/src/main/scala/org/apache/spark/scheduler/cluster/k8s/ExecutorPodsAllocator.scala
+++ resource-managers/kubernetes/core/src/main/scala/org/apache/spark/scheduler/cluster/k8s/ExecutorPodsAllocator.scala
@@ -21,6 +21,7 @@ import java.util.concurrent.atomic.{AtomicInteger, AtomicLong}
 import io.fabric8.kubernetes.api.model.PodBuilder
 import io.fabric8.kubernetes.client.KubernetesClient
 import scala.collection.mutable
+import scala.util.Try
 
 import org.apache.spark.{SparkConf, SparkException}
 import org.apache.spark.deploy.k8s.Config._
@@ -51,13 +52,31 @@ private[spark] class ExecutorPodsAllocator(
   private val kubernetesDriverPodName = conf
     .get(KUBERNETES_DRIVER_POD_NAME)
 
+  // Retry 300 times waiting 2 seconds (10 minutes)
+  private def getDriverPodWithRetries(name: String) = {
+
+    def getDriverPod(retriesLeft: Int) = {
+      if (retriesLeft == 0) None
+      else Try {
+        kubernetesClient.pods()
+          .withName(name)
+          .get()
+      } recover {
+        case e: Exception =>
+          logWarn(s"Couldn't get Spark Driver pod. Trying again in 2 seconds. $retriesLeft retries left.", e)
+          Thread.sleep(2000)
+          getDriverPod(retriesLeft - 1)
+      }.toOption
+    }
+
+    getDriverPod(300)
+  }
+
   private val driverPod = kubernetesDriverPodName
-    .map(name => Option(kubernetesClient.pods()
-      .withName(name)
-      .get())
-      .getOrElse(throw new SparkException(
-        s"No pod was found named $kubernetesDriverPodName in the cluster in the " +
-          s"namespace $namespace (this was supposed to be the driver pod.).")))
+    .map(name => getDriverPodWithRetries(name))
+    .getOrElse(throw new SparkException(
+      s"No pod was found named $kubernetesDriverPodName in the cluster in the " +
+        s"namespace $namespace (this was supposed to be the driver pod.).")))
 
   // Executor IDs that have been requested from Kubernetes but have not been detected in any
   // snapshot yet. Mapped to the timestamp when they were created.
@@ -85,7 +104,7 @@ private[spark] class ExecutorPodsAllocator(
         logWarning(s"Executor with id $execId was not detected in the Kubernetes" +
           s" cluster after $podCreationTimeout milliseconds despite the fact that a" +
           " previous allocation attempt tried to create it. The executor may have been" +
-          " deleted but the application missed the deletion event.")
+          s" deleted but the application missed the deletion event. TimeCreated: $timeCreated")
         Utils.tryLogNonFatalError {
           kubernetesClient
             .pods()
@@ -96,8 +115,8 @@ private[spark] class ExecutorPodsAllocator(
         }
         newlyCreatedExecutors -= execId
       } else {
-        logDebug(s"Executor with id $execId was not found in the Kubernetes cluster since it" +
-          s" was created ${currentTime - timeCreated} milliseconds ago.")
+        logInfo(s"Executor with id $execId was not found in the Kubernetes cluster since it" +
+          s" was created ${currentTime - timeCreated} milliseconds ago. TimeCreated: $timeCreated")
       }
     }
 
