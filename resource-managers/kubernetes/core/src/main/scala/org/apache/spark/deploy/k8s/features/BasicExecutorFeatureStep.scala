cb64de84c2287f10f60b3c01e5ffe01c254f2c97
Yikun Jiang <yikunkero@gmail.com>
2021-07-18 15:59:34 -0700

    [SPARK-36075][K8S] Support for specifiying executor/driver node selector

--- resource-managers/kubernetes/core/src/main/scala/org/apache/spark/deploy/k8s/features/BasicExecutorFeatureStep.scala
+++ resource-managers/kubernetes/core/src/main/scala/org/apache/spark/deploy/k8s/features/BasicExecutorFeatureStep.scala
@@ -262,6 +262,7 @@ private[spark] class BasicExecutorFeatureStep(
         .withHostname(hostname)
         .withRestartPolicy("Never")
         .addToNodeSelector(kubernetesConf.nodeSelector.asJava)
+        .addToNodeSelector(kubernetesConf.executorNodeSelector.asJava)
         .addToImagePullSecrets(kubernetesConf.imagePullSecrets: _*)
         .addNewVolume()
           .withName(SPARK_CONF_VOLUME_EXEC)
